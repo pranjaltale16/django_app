43e7d4009af18a01964352f65136b1c61cba7595
pranjaltale16 <pranjaltale16@gmail.com>
2016-07-07 19:50:56 +0530

    now a user can vote for a particular question only once in a particluar logged in session

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -23,19 +23,26 @@ class ResultsView(generic.DetailView):
 
 
 def vote(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
 	try:
-		selected_choice = question.choice_set.get(pk=request.POST['choice'])
-	except (KeyError, Choice.DoesNotExist):
-	# Redisplay the question voting form.
-		return render(request, 'polls/detail.html', {
-			'question': question,
-			'error_message': "You didn't select a choice.",
-		})
-	else:
-		selected_choice.votes += 1
-		selected_choice.save()
-		# Always return an HttpResponseRedirect after successfully dealing
-		# with POST data. This prevents data from being posted twice if a
-		# user hits the Back button.
-		return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+		if request.session[question_id] :
+			q = "you have already voted for that question"
+			context = {'q':q }
+			return render(request,'polls/voted.html',context)
+	except:
+		question = get_object_or_404(Question, pk=question_id)
+		try:
+			selected_choice = question.choice_set.get(pk=request.POST['choice'])
+		except (KeyError, Choice.DoesNotExist):
+		# Redisplay the question voting form.
+			return render(request, 'polls/detail.html', {
+				'question': question,
+				'error_message': "You didn't select a choice.",
+			})
+		else:
+			request.session[question_id] = True
+			selected_choice.votes += 1
+			selected_choice.save()
+			# Always return an HttpResponseRedirect after successfully dealing
+			# with POST data. This prevents data from being posted twice if a
+			# user hits the Back button.
+			return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
